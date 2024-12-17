79f609fdb5f054058c1fde328a06510cd93e62ca
Manish Kumar Barman <manishkumarbarman111@gmail.com>
2024-12-18 01:35:22 +0530

    float 2 decimal value

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function updateLocalStorage() {
 }
 
 function updateBalance() {
-    const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+    const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0).toFixed(2);
     balanceElement.textContent = `Current Balance: ₹${balance}`;
     balanceElement.style.backgroundColor = balance < warningAmount ? '#dc3545' : '#28a745';
 }
